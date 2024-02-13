02ef836ffc1fde6d5a6bf0a8b05effd6c2667876
jiwasantuy1815 <122097323+jiwasantuy1815@users.noreply.github.com>
2024-02-13 23:25:35 +0800

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -6,6 +6,44 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const offlineFallbackPage = "ToDo-replace-this-name.html";
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('my-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css',
+        '/script.js',
+        'https://cdn.jsdelivr.net/gh/jettheme/js@0.5.5/main.js',
+        'https://cdn.jsdelivr.net/gh/jiwasantuy1815/webnalen@main/safari-pinned-tab.svg',
+        'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css',
+        'https://cdn.jsdelivr.net/gh/fontawesome-icon/icon@main/new.js',
+        'https://cdn.jsdelivr.net/gh/fontawesome-icon/icon@main/new.js',
+        'https://cdn.jsdelivr.net/gh/jiwasantuy1815/webnalen@main/apple-touch-icon.png',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('push', (event) => {
+  const data = event.data.json();
+  const title = data.title;
+  const body = data.body;
+
+  self.registration.showNotification(title, {
+    body: body,
+    icon: 'https://cdn.jsdelivr.net/gh/jiwasantuy1815/webnalen@main/apple-touch-icon.png',
+  });
+});
+
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
